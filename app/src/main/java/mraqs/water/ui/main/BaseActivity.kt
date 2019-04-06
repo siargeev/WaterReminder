89c2cd63ac3c38d6c8457b1b9dde8de4b950ef17
Alexander Siargeev <49080233+siargeev@users.noreply.github.com>
2019-04-06 10:05:30 +0300

    Add Custom Switch

--- app/src/main/java/mraqs/water/ui/main/BaseActivity.kt
+++ app/src/main/java/mraqs/water/ui/main/BaseActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import com.google.android.material.bottomnavigation.LabelVisibilityMode
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.bottom_navigation_view.bottom_navigation_view
-import mraqs.water.R
 import mraqs.water.ui.main.home.HomeActivity
 import mraqs.water.ui.main.settings.SettingsActivity
 import javax.inject.Inject
@@ -27,13 +26,19 @@ open class BaseActivity @Inject constructor(private val navItem: Int) : DaggerAp
 
             setOnNavigationItemSelectedListener {
                 val nextActivity = when (it.itemId) {
-                    R.id.nav_item_home -> HomeActivity::class.java
-                    R.id.nav_item_settings -> SettingsActivity::class.java
+                    mraqs.water.R.id.nav_item_home -> {
+                        if (currentItem != 0) HomeActivity::class
+                        else null
+                    }
+                    mraqs.water.R.id.nav_item_settings -> {
+                        if (currentItem != 1) SettingsActivity::class
+                        else null
+                    }
                     else -> null
                 }
 
                 if (nextActivity != null) {
-                    val intent = Intent(this@BaseActivity, nextActivity)
+                    val intent = Intent(this@BaseActivity, nextActivity.java)
                     intent.flags = Intent.FLAG_ACTIVITY_NO_ANIMATION
                     intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
                     intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
@@ -43,7 +48,7 @@ open class BaseActivity @Inject constructor(private val navItem: Int) : DaggerAp
                     overridePendingTransition(0, 0)
                     finish()
                 }
-                false
+                true
             }
         }
     }
